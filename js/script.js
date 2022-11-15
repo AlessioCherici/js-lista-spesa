2191c69b642cf9126132aa6797eabf94be60830c
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-15 14:24:46 +0100

    Creati file + inizio layout HTML

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*Data una lista della spesa già compilata nel vostro codice, stampare in console gli elementi della lista individualmente, prima con un ciclo for, poi con un ciclo while.
+Quando le stampe in console sono corrette, stampare gli elementi in un elenco puntato in HTML, utilizzando uno dei due cicli scritti al punto precedente.*/
+
+
+let arraySpesa = ["Pane","Uova","Latte","Pesto","Formaggio","Caffè","Tritolo","Shampoo","Miele","Miele","Taralli","Dentifricio","Sacchi della spazzatura"];
+
+
