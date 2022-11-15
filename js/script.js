cf5e346722d3a9454f2449efa2ef647495015cdb
AlessioCherici <alessio.cherici@hotmail.it>
2022-11-15 15:42:38 +0100

    creato codice javascript + bonus *fine*

--- js/script.js
+++ js/script.js
@@ -1,7 +1,34 @@
 /*Data una lista della spesa già compilata nel vostro codice, stampare in console gli elementi della lista individualmente, prima con un ciclo for, poi con un ciclo while.
 Quando le stampe in console sono corrette, stampare gli elementi in un elenco puntato in HTML, utilizzando uno dei due cicli scritti al punto precedente.*/
 
+//Creo array 
+let arraySpesa = ["Pane","Uova","Latte","Pesto","Formaggio","Caffè","Tritolo","Shampoo","Miele","Una gioia","Taralli","Dentifricio","Sacchi della spazzatura"];
 
-let arraySpesa = ["Pane","Uova","Latte","Pesto","Formaggio","Caffè","Tritolo","Shampoo","Miele","Miele","Taralli","Dentifricio","Sacchi della spazzatura"];
+let i = 0;
+while (i < arraySpesa.length){
+i++;
+console.log(arraySpesa[i] + "Stampato da ciclo while");
+}
+
+//Creo ciclo for per printare in console i valori di array
+for (let i = 0; i < arraySpesa.length; i++){
+
+    console.log(arraySpesa[i] + "Stampato da ciclo for");
+
+    document.getElementById("ulSpesa").innerHTML += '<li id="itemLista">'+arraySpesa[i]+'</li>';
+}
+//creo codice per identificare e rendere funzionale il bottone
+let bottoneAggiunte = document.getElementById("bottone");
+
+bottoneAggiunte.addEventListener("click",function(){
+
+    let aggiuntaSpesa = document.getElementById("inputSpesa").value;
+    arraySpesa.push(aggiuntaSpesa);
+
+    document.getElementById("ulSpesa").innerHTML += '<li id="itemLista">'+aggiuntaSpesa+'</li>';
+
+    console.log(aggiuntaSpesa);
+
+});
 
 
